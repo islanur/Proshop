0c1fb8cef26b702badbc42eab4faa4399a6d8d9e
Islanur <islanur@gmail.com>
2021-05-14 23:17:55 +0800

    Mengambil data dari server (express)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main>
-        <h1>Welcome to PROSHOP</h1>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
