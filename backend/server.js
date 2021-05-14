922b327886c88a348ab977247bd79e1541bafe28
Islanur <islanur@gmail.com>
2021-05-15 06:07:32 +0800

    Koneksi ke database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 /* eslint-disable no-undef */
 import express from 'express';
 import dotenv from 'dotenv';
+// eslint-disable-next-line no-unused-vars
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,5 +29,6 @@ app.get('/api/products/:id', (req, res) => {
 app.listen(port, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode and listening at http://localhost:${port}`
+      .yellow.bold
   );
 });
