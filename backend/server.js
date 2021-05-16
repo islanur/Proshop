a5693d3b89e53701467c8d0dc8d969b95baa1b37
Islanur <islanur@gmail.com>
2021-05-17 00:34:04 +0800

    User Controller

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
-// import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -14,14 +14,14 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running....');
 });
 
 app.use('/api/products', productRoutes);
-// app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
