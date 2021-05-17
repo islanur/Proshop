40d9347bb9ebf7cf2946384c65794f8e73513b75
Islanur <islanur@gmail.com>
2021-05-17 17:34:46 +0800

    Update user Profile

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-unused-vars */
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
